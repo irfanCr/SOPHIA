aa548fc374905f5de1edab32895733a3a272a448
Hisham <86567889+Ichukarikk@users.noreply.github.com>
2021-06-30 16:16:39 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -557,10 +557,16 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
             // Hoşgeldin Mesajı
             var gb = await getMessage(msg.key.remoteJid);
             if (gb !== false) {
-                await conn.sendMessage(msg.key.remoteJid, gb.message, MessageType.text);
-            }
-            return;
-        }
+           let pp
+
+                try { pp = await conn.getProfilePicture(msg.messageStubParameters[0]); } catch { pp = await conn.getProfilePicture(); }
+
+                await axios.get(pp, {responseType: 'arraybuffer'}).then(async (res) => {
+
+                await conn.sendMessage(msg.key.remoteJid, res.data, MessageType.image, {caption:
+   
+     
+     
         // ==================== End Greetings ====================
 
         // ==================== Blocked Chats ====================
